0f1f154033d8300555abb6140bcd56a59871dfc2
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-03-12 21:40:52 +0800

    Update configure_beast.sh

--- configure_beast.sh
+++ configure_beast.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cat > /etc/default/beast-splitter << EOF
-# Generated automatically by fa_config_generator
-# This file will be overwritten on reboot.
 ENABLED=yes
 INPUT_OPTIONS="--serial /dev/beast --fixed-baud 1000000 "
 OUTPUT_OPTIONS="--listen 30005:R --connect localhost:30104:R"
