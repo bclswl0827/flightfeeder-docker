6f0cd7c49e602787fce5ec83d9fd3f1ee74bbf8c
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-03-13 08:53:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -11,7 +11,3 @@ NET_OPTIONS="--net --net-heartbeat 60 --net-ro-size 1000 --net-ro-interval 1 --n
 JSON_OPTIONS="--json-location-accuracy 2"
 RECEIVER_OPTIONS="--net-only --net-bo-port 0 --fix"
 EOF
-service beast-splitter start
-service dump1090-fa start
-lighty-enable-mod dump1090-fa
-service lighttpd force-reload
