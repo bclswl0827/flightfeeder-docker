d6deb8cf3fc0ff2dcfd598055a1df8f2cd2b2127
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-03-13 10:39:08 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cat > /etc/default/beast-splitter << EOF
-ENABLED=yes
-INPUT_OPTIONS="--serial /dev/beast --fixed-baud 1000000 "
+ENABLED="yes"
+INPUT_OPTIONS="--serial /dev/beast --fixed-baud 1000000"
 OUTPUT_OPTIONS="--listen 30005:R --connect localhost:30104:R"
 EOF
 cat > /etc/default/dump1090-fa << EOF
-ENABLED=yes
+ENABLED="yes"
 DECODER_OPTIONS=" --lat $LAT --lon $LON --max-range 360"
 NET_OPTIONS="--net --net-heartbeat 60 --net-ro-size 1000 --net-ro-interval 1 --net-http-port 0 --net-ri-port 0 --net-ro-port 30002 --net-sbs-port 30003 --net-bi-port 30004,30104"
 JSON_OPTIONS="--json-location-accuracy 2"
