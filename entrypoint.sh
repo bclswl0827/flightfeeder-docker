28b818d4dc5d36159467c1195f82f3a3719f61ab
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-05-17 12:04:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,13 @@ if [ -z "$LON" ]; then
 	exit 1
 fi
 
-sed -i "s/<env1>/$LAT/g" /etc/default/dump1090-fa
-sed -i "s/<env2>/$LON/g" /etc/default/dump1090-fa
+cat << EOF > /etc/default/dump1090-fa
+ENABLED=yes
+DECODER_OPTIONS="--lat $LAT --lon $LON --max-range 360"
+NET_OPTIONS="--net --net-heartbeat 60 --net-ro-size 1000 --net-ro-interval 1 --net-http-port 0 --net-ri-port 0 --net-ro-port 30002 --net-sbs-port 30003 --net-bi-port 30004,30104"
+JSON_OPTIONS="--json-location-accuracy 2"
+RECEIVER_OPTIONS="--net-only --net-bo-port 0 --fix"
+EOF
+
+/usr/share/beast-splitter/start-beast-splitter --status-file %t/beast-splitter/status.json >/dev/null  2>&1  &
+/usr/share/dump1090-fa/start-dump1090-fa --write-json %t/dump1090-fa --quiet >/dev/null  2>&1  &
